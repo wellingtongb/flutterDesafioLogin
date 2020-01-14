e95b9a01c2f1dd509df6d5a9435fc20acbfe84b8
Wellington Gomes Bezerra <A70996@gabril.com.br>
2020-01-14 19:21:54 -0300

    primeiro commit

---
+++ lib/login_challenge.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() => runApp(LoginApp());
+
+class LoginApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Login Challenge',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple
+      ),
+      home: LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _LoginPageState();
+  }
+}
+
+class _LoginPageState extends State<LoginPage> {
+
+  // f45d27
+  // f5851f
+
+  @override
+  void initState() {
+    SystemChrome.setEnabledSystemUIOverlays([]);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:desafio_login/login_challenge.dart';
+
+void main() => runApp( LoginApp());
